12843860eaf8658f635a685c737b9d2602d0a6eb
pa733 <2291750588@qq.com>
2023-01-08 23:31:02 +0800

    Create build.yml

--- Program.cs
+++ Program.cs
@@ -25,18 +25,18 @@ namespace MhyProt2Drv
             }
             else
             {
-                Console.WriteLine("Enuming module of csrss.exe");
+                Console.WriteLine("Enuming module of avp.exe");
                 uint pid = (uint)Process.GetProcessesByName("csrss")[0].Id;
                 List<MhyProtEnumModule> m = mhyprot.EnumProcessModule(pid);
                 IntPtr baseAddr = IntPtr.Zero;
                 foreach(MhyProtEnumModule sm in m)
                 {
                     Console.WriteLine("ModuleName: " + sm.ModuleName + " ModulePath:" + sm.ModulePath + " BaseAddress:0x" + sm.BaseAddress.ToString("x2") + " Size:0x" + sm.SizeOfImage.ToString("x2"));
-                    if (sm.ModuleName == "csrss.exe") baseAddr = sm.BaseAddress;
+                    if (sm.ModuleName == "avp.exe") baseAddr = sm.BaseAddress;
                 }
                 Memory mem = new Memory(mhyprot, pid);
                 long currentTicks = DateTime.Now.Ticks;
-                Console.WriteLine("Reading memory of csrss.exe");
+                Console.WriteLine("Reading memory of avp.exe");
                 for (int i = 0; i < 1000; i++)
                 {
                     mem.Read(baseAddr, 1024);
